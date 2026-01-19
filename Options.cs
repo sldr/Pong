c1d991dfd8acb84c700ec99f5103ba74e79280b8
garrettderudder <garrettd1303@gmail.com>
2026-01-19 00:18:42 -0600

    Fixed audio bugs

--- Options.cs
+++ Options.cs
@@ -22,6 +22,13 @@ public partial class Options : Node2D
 			MainVolMuted = optRes.MainVolMuted;
 			MusicVolMuted = optRes.MusicVolMuted;
 			SFXVolMuted = optRes.SFXVolMuted;
+			// Apply all saved states to audio server
+			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Master"), MainVolumeDb);
+			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Music"), MusicVolumeDb);
+			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Sfx"), SFXVolumeDb);
+			AudioServer.SetBusMute(AudioServer.GetBusIndex("Master"), MainVolMuted);
+			AudioServer.SetBusMute(AudioServer.GetBusIndex("Music"), MusicVolMuted);
+			AudioServer.SetBusMute(AudioServer.GetBusIndex("Sfx"), SFXVolMuted);
 			// Apply all saved states to sliders
 			GetNode<Slider>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/MainVol/MainVolSlider").Value = MainVolumeDb;
 			GetNode<Slider>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/MusicVol/MusicVolSlider").Value = MusicVolumeDb;
@@ -39,9 +46,30 @@ public partial class Options : Node2D
 			MainVolMuted = optRes.DefaultMainVolMuted;
 			MusicVolMuted = optRes.DefaultMusicVolMuted;
 			SFXVolMuted = optRes.DefaultSFXVolMuted;
+			// Apply all default states to audio server
+			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Master"), MainVolumeDb);
+			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Music"), MusicVolumeDb);
+			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Sfx"), SFXVolumeDb);
+			AudioServer.SetBusMute(AudioServer.GetBusIndex("Master"), MainVolMuted);
+			AudioServer.SetBusMute(AudioServer.GetBusIndex("Music"), MusicVolMuted);
+			AudioServer.SetBusMute(AudioServer.GetBusIndex("Sfx"), SFXVolMuted);
+			// Apply all default states to sliders
+			GetNode<Slider>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/MainVol/MainVolSlider").Value = MainVolumeDb;
+			GetNode<Slider>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/MusicVol/MusicVolSlider").Value = MusicVolumeDb;
+			GetNode<Slider>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/SfxVol/SfxVolSlider").Value = SFXVolumeDb;
+			GetNode<BaseButton>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/MainVol/MainVolMute").ButtonPressed = MainVolMuted;
+			GetNode<BaseButton>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/MusicVol/MusicVolMute").ButtonPressed = MusicVolMuted;
+			GetNode<BaseButton>("Menu/AspectRatioContainer/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/SfxVol/SfxVolMute").ButtonPressed = SFXVolMuted;
 		}
 	}
-
+#region Button Signals
+	/// <summary>
+	/// Each button signal changes the value of the local variable and updates the audio server to listen as a preview for the user.
+	/// Reset buttons set the sliders to the defaults defined in the Options Resource, which in return updates the corresponding signal.
+	/// The save button simply updates the Options Resource Stored Volume Settings.
+	/// The exit button loads the saved settings and applies it to the audio server, therefore reverting any unsaved changes.
+	/// </summary>
+	/// <param name="value"></param>
 	private void _on_main_vol_slider_value_changed(float value)
 	{
 		MainVolumeDb = value;
@@ -108,8 +136,23 @@ public partial class Options : Node2D
 		optRes.isSettingsSaved = true;
 	}
 
-		public void _on_exit_pressed()
+	public void _on_exit_pressed()
 	{
+		// Load saved settings
+		MainVolumeDb = optRes.MainVolumeDb;
+		MusicVolumeDb = optRes.MusicVolumeDb;
+		SFXVolumeDb = optRes.SFXVolumeDb;
+		MainVolMuted = optRes.MainVolMuted;
+		MusicVolMuted = optRes.MusicVolMuted;
+		SFXVolMuted = optRes.SFXVolMuted;
+		// Apply all saved states to audio server
+		AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Master"), MainVolumeDb);
+		AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Music"), MusicVolumeDb);
+		AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Sfx"), SFXVolumeDb);
+		AudioServer.SetBusMute(AudioServer.GetBusIndex("Master"), MainVolMuted);
+		AudioServer.SetBusMute(AudioServer.GetBusIndex("Music"), MusicVolMuted);
+		AudioServer.SetBusMute(AudioServer.GetBusIndex("Sfx"), SFXVolMuted);
 		GetTree().ChangeSceneToFile("res://Main.tscn");
 	}
+#endregion
 }
